5a13f05f3abb1b672c9933e3fd376e2ad0040c39
Monika Musali <94759221+MONIKmm@users.noreply.github.com>
2026-05-11 15:54:53 +0530

    Delete PMC_PF directory

--- PMC_PF/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  console.log("PMC Founder Portfolio loaded");
-});
-document.addEventListener("contextmenu", function(e) {
-  e.preventDefault();
-});
